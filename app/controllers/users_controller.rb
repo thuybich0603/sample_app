26a2d8d5e09355e9c809e8bf1bbc4694e9856f55
thuybich0603 <thuybichtruong06031998@gmail.com>
2018-11-23 15:19:05 +0700

    Add hello
    Add a Static Pages controller
    
    finish chapter_3
    
    sualoi
    
    fixbug
    
    suatext
    
    fix
    
    sualoiconflict
    
    suatiep
    
    finished chapter5
    
    sua
    
    da sua
    
    finshed chap_7
    
    fix chapter_7
    
    sua
    
    chap7
    
    finished chap8
    
    fix
    
    sua_tiep
    
    end file
    
    finished chapter_9
    
    hoan thanh chapter_9
    
    finished chapter_10
    
    sua
    
    sua xong
    
    sua xong
    
    chap10
    
    chapter_1
    
    finished chapter_11
    
    suaxong
    
    fix lan cuoi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t ".success"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t "users.create.check_email"
+      redirect_to root_path
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,23 +31,33 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
 
-   private
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
-    def downcase_email
-      self.email = email.downcase
-    end
+  def activate
+    update_attribute :activated, true
+    update_attribute :activated_at, Time.zone.now
+  end
 
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
