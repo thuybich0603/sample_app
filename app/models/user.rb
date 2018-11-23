3932e97ae4e58bf394324de7722bbe9d9c70479c
thuybich0603 <thuybichtruong06031998@gmail.com>
2018-11-23 15:19:05 +0700

    Add hello
    Add a Static Pages controller
    
    finish chapter_3
    
    sualoi
    
    fixbug
    
    suatext
    
    fix
    
    sualoiconflict
    
    suatiep
    
    finished chapter5
    
    sua
    
    da sua
    
    finshed chap_7
    
    fix chapter_7
    
    sua
    
    chap7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   before_save{email.downcase!}
-  validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length:
+  {maximum: Settings.users.name_maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
+  validates :email, presence: true, length:
+  {maximum: Settings.users.email_maximum},
   format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length:
+  {minimum: Settings.users.pass_minimum}
 end
